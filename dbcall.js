266605dba948e586de8013fbc82a07e66464846d
Per Lundholm <per@artcomputer.se>
2023-11-11 19:07:58 +0100

    WIP

--- dbcall.js
+++ dbcall.js
@@ -21,11 +21,9 @@ const systemPrompt = `
     Chinook Music Database.
     `;
 
-let databaseSchemaString = "";
-
 const sqlite3 = require('sqlite3').verbose();
 
-let conn = new sqlite3.Database('data/Chinook.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('data/Chinook.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
     } else {
@@ -69,11 +67,16 @@ async function getDatabaseInfo(db) {
     }));
 }
 
-const databaseSchemaDict = getDatabaseInfo(conn);
+async function getSchema(conn) {
+    let schema = "";
+    const databaseSchemaDict = await getDatabaseInfo(conn);
+    databaseSchemaDict.forEach(table => {
+        schema += `Table: ${table.tableName}\nColumns: ${table.columnNames.join(', ')}\n`;
+    });
+    return schema;
+}
 
-databaseSchemaDict.forEach(table => {
-    databaseSchemaString += `Table: ${table.table_name}\nColumns: ${table.column_names.join(', ')}\n`;
-});
+const databaseSchemaString = getSchema(db);
 
 const tools = [
     {
@@ -103,39 +106,109 @@ const tools = [
     }
 ]
 
-const chatWithGPT = async () => {
-    const messages = [{
-        role: "system",
-        content: systemPrompt
-    }];
-    while (true) {
-        const userInput = await new Promise(resolve => rl.question("You: ", resolve));
-
-        if (userInput.toLowerCase() === "exit") {
-            rl.close();
-            return;
-        }
-
-        messages.push({role: "user", content: userInput});
-
-        const completion = await openai.chat.completions.create({
-            model: "gpt-4-1106-preview",
-            messages: messages,
-            temperature: 0.5,
-            stream: true
-        });
-
-        let response = "";
-        for await (const chunk of completion) {
-            if (("content" in chunk.choices[0].delta)) {
-                response += chunk.choices[0].delta.content;
-                process.stdout.write(chunk.choices[0].delta.content);
-            }
-        }
-        messages.push({role: "assistant", content: response});
-    }
-};
-
 function executeQuery(query) {
-
+    return new Promise((resolve, reject) => {
+        let result = "";
+        db.serialize(() => {
+            db.each(query, (err, row) => {
+                if (err) {
+                    console.error(err.message);
+                    reject(err.message);
+                } else {
+                    result += JSON.stringify(row) + "\n"; // Convert row object to string
+                }
+            }, (err, count) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    });
 }
+
+
+const chatWithGPT = async () => {
+        const messages = [{
+            role: "system",
+            content: systemPrompt
+        }];
+        while (true) {
+            const userInput = await new Promise(resolve => rl.question("\nYou: ", resolve));
+
+            if (userInput.toLowerCase() === "exit") {
+                rl.close();
+                return;
+            }
+
+            messages.push({role: "user", content: userInput});
+
+            const completion = await openai.chat.completions.create({
+                model: "gpt-4-1106-preview",
+                tools: tools,
+                messages: messages,
+                temperature: 0.5,
+                stream: true
+            });
+
+            let response = "";
+            let functionArgs = "";
+            let functionName = "";
+            let toolCallId = undefined;
+            for await (const chunk of completion) {
+                if (chunk.choices[0].delta.tool_calls) {
+                    for (const toolCall of chunk.choices[0].delta.tool_calls) {
+                        if (toolCall.id) {
+                            toolCallId = toolCall.id
+                        }
+                        if (toolCall.function.name) {
+                            functionName += toolCall.function.name;
+                        }
+                        if (toolCall.function.arguments) {
+                            functionArgs += toolCall.function.arguments;
+                        }
+                    }
+                } else if (("content" in chunk.choices[0].delta)) {
+                    response += chunk.choices[0].delta.content;
+                    process.stdout.write(chunk.choices[0].delta.content);
+                }
+            }
+            if (functionName.length < 1) {
+                messages.push({role: "assistant", content: response});
+            } else {
+                const fn = JSON.parse(functionArgs);
+                const functionResponse = await executeQuery(fn.query).then(result => {
+                    return result;
+                }).catch(error => {
+                    return error;
+                });
+                process.stdout.write(functionResponse);
+
+                messages.push({
+                        role: "assistant",
+                        tool_calls: [
+                            {
+                                id: toolCallId,
+                                type : "function",
+                                function: {
+                                    name: functionName,
+                                    arguments: functionArgs
+                                }
+                            }
+                        ]
+                    }
+                );
+                messages.push({
+                    tool_call_id: toolCallId,
+                    role: "tool",
+                    name: functionName,
+                    content: functionResponse,
+                });
+            }
+
+        }
+    }
+;
+
+chatWithGPT();
